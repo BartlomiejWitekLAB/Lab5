c34f3cec7c69f5a73f1006fee8ef65ebaa101f7e
73927 <73927@student.wsiz.edu.pl>
2025-12-25 11:56:50 +0100

    .

--- Lab_zad3.py
+++ Lab_zad3.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+df = pd.read_csv('demografia.csv', na_values='.')
+df['KRAJE'] = df['KRAJE'].str.strip()
+df.set_index('KRAJE', inplace=True)
+df.columns = df.columns.str.replace('a', '')
+max_w_roku = df.max()
+szukany_rok = max_w_roku.idxmax()
+szukany_kraj = df[szukany_rok].idxmax()
+najwieksza_wartosc = df.loc[szukany_kraj, szukany_rok]
+print(f"Największy przyrost ludności: {najwieksza_wartosc}")
+print(f"Kraj: {szukany_kraj}")
+print(f"Rok: {szukany_rok}")
